de12c753fbec9a87fc4b28105847f4293b490964
Andreina-Romero <andreina.romero.garcia@gmail.com>
2019-09-04 23:42:06 +0200

    Install scss compilator. Add styles on App.scss

--- src/App.js
+++ src/App.js
@@ -15,10 +15,26 @@ function App() {
             Kyary Pamyu pamyu
           </h1>
           <p className="card__info-date">
-            Kyary Pamyu Pamyu (きゃりーぱみゅぱみゅ Kyarī Pamyu Pamyu?, Nishitōkyō, Tokio, 29 de enero de 1993), cuyo verdadero nombre es Kiriko Takemura (竹村 桐子 Takemura Kiriko?), es una modelo, blogger y cantante japonesa.1​ Su música es producida por Yasutaka Nakata, fundador del grupo Capsule.
+           Hola! Hoy es 4 de Septiembre del 2019
           </p>
         </div>
       </header>
+
+      <main className="card__main">
+        <p className="main__content">
+          Kyary Pamyu Pamyu (きゃりーぱみゅぱみゅ Kyarī Pamyu Pamyu?, Nishitōkyō, Tokio, 29 de enero de 1993), cuyo verdadero nombre es Kiriko Takemura (竹村 桐子 Takemura Kiriko?), es una modelo, blogger y cantante japonesa.1​ Su música es producida por Yasutaka Nakata, fundador del grupo Capsule.
+        </p>
+
+        <div className="main__more-info">
+            <a href="#link" className="more-info__link">
+              Leer más...
+            </a>
+            <div className="likes">
+              <span className="likes__number">37</span>
+              <i className="fas fa-heart"></i>
+            </div>
+        </div>
+      </main>
       </div>
     </div>
   );
