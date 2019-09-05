59896e02b58dc5fb3a5ca65a9b67d7840bd09ff2
Andreina-Romero <andreina.romero.garcia@gmail.com>
2019-09-05 23:10:44 +0200

    Change MediaCard.js to functional component

--- src/components/MediaCard.js
+++ src/components/MediaCard.js
@@ -3,8 +3,7 @@ import HeaderCard from './HeaderCard';
 import MainCard from './MainCard';
 import kyaryPhoto from '../images/kyary2.jpg';
 
-class MediaCard extends React.Component {
-  render() {
+const MediaCard = props => {
     const cardUserName = "Kyary Pamyu pamyu";
     const date = "Hola! Hoy es 4 de Septiembre de 2019";
     const cardDescription = "Kyary Pamyu Pamyu (きゃりーぱみゅぱみゅ Kyarī Pamyu Pamyu?, Nishitōkyō, Tokio, 29 de enero de 1993), cuyo verdadero nombre es Kiriko Takemura (竹村 桐子 Takemura Kiriko?), es una modelo, blogger y cantante japonesa.1​ Su música es producida por Yasutaka Nakata, fundador del grupo Capsule."
@@ -26,7 +25,7 @@ class MediaCard extends React.Component {
         </div>
       </div>
     );
-    }
+  
   }
 
 export default MediaCard;
